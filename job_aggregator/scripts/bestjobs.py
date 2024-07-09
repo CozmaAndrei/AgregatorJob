7e18d5477151ce655a83d6d8abd953190b02f919
Ubuntu <ubuntu@ip-172-31-16-150.eu-central-1.compute.internal>
2024-07-09 15:43:41 +0000

    added paid proxy

--- job_aggregator/scripts/bestjobs.py
+++ job_aggregator/scripts/bestjobs.py
@@ -1,22 +1,29 @@
 from jobs.models import Jobs
 import requests
+from decouple import config
+from requests.auth import HTTPProxyAuth
 from bs4 import BeautifulSoup
         
 def jobs_on_bestjobs():
-    print("bestjobs ruleaza")
+    # print("bestjobs ruleaza")
     page = 1
-    while page <= 50:
-        print(f'pagina {page}')
+    while page <= 200:
+        # print(f'pagina {page}')
         url = 'https://www.bestjobs.eu/ro/locuri-de-munca/' + str(page)
-        print("se citeste url")
+        # print("se citeste url")
+        proxy_ip = config('PROXY_IP')
+        proxy_port = config('PROXY_PORT')
+        username = config('USERNAME')
+        password = config('PASSWORD')
         proxies = {
-            'http': 'http://proxy_ip:proxy_port',
-            'https': 'https://proxy_ip:proxy_port',
+            'http': f'http://{username}:{password}@{proxy_ip}:{proxy_port}',
+            'https': f'http://{username}:{password}@{proxy_ip}:{proxy_port}',
         }
+        auth = HTTPProxyAuth(username, password)
         source = requests.get(url, proxies=proxies)
-        print(source)
+        # print(source)
         if source.status_code == 200:
-            print("statusul este 200!")
+            # print("statusul este 200!")
             soup = BeautifulSoup(source.content, 'html.parser')
             jobs_list = soup.find_all('div', class_='list-card')
             for job in jobs_list:
@@ -29,15 +36,17 @@ def jobs_on_bestjobs():
                 else:
                     location = 'Necunoscuta'
                 
-                if 'python' in jobs_title.lower():
+                if 'lucrator' in jobs_title.lower():
                     # if not Jobs.objects.filter(url=url).exists():
                     Jobs.objects.create(title=jobs_title, company=jobs_company, url=url, location=location)
+                    print(f"Am găsit un job: {jobs_title} - {jobs_company} - {location}")
                 else:
                     pass
             
             page += 1
         else:
-            print("statusul nu este 200!")
+            pass
+            # print("statusul nu este 200!")
         
 if __name__ == '__main__':
     print("__name__==__main__ ruleaza in bestjobs")
